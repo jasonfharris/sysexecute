6278d553adb8e50a3cd392dca19493c454c9abe6
Jason Harris <jason@jasonfharris.com>
2016-11-04 12:40:03 +0100

    Bump the version number to a release number.

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'sysexecute',
   packages = ['sysexecute'], # this must be the same as the name above
-  version = '0.1',
+  version = '1.0.0',
   description = 'A library for simplified executing of system commands',
   author = 'Jason Harris',
   author_email = 'jason@unifiedthought.com',
   url = 'https://github.com/jasonfharris/sysexecute',
-  download_url = 'https://github.com/jasonfharris/sysexecute/tarball/0.1', # I'll explain this in a second
+  download_url = 'https://github.com/jasonfharris/sysexecute/tarball/1.0.0', # I'll explain this in a second
   keywords = ['execute', 'shell', 'system'],
   classifiers = [],
 )
