5caec7a82e37780fa86019377993715bf3f1e22c
Jason Harris <jason@jasonfharris.com>
2016-11-05 19:15:42 +0100

    Bump the revision to 1.0.2 for new additions

--- setup.py
+++ setup.py
@@ -2,20 +2,26 @@ from distutils.core import setup
 setup(
     name = 'sysexecute',
     packages = ['sysexecute'], # this must be the same as the name above
-    version = '1.0.1',
+    version = '1.0.2',
     description = 'A library for simplified executing of system commands',
     author = 'Jason Harris',
     author_email = 'jason@jasonfharris.com',
     license='MIT',
     url = 'https://github.com/jasonfharris/sysexecute',
-    download_url = 'https://github.com/jasonfharris/sysexecute/tarball/1.0.1', # I'll explain this in a second
+    download_url = 'https://github.com/jasonfharris/sysexecute/tarball/1.0.2', # I'll explain this in a second
     keywords = ['execute', 'shell', 'system'],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
 
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
-        'Topic :: Software Development :: Utilities',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Networking',
+        'Topic :: System :: Shells',
+        'Topic :: System :: Systems Administration',
+        'Topic :: Utilities',
 
         # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: MIT License',
